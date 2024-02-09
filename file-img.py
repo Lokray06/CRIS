44e6461f0e97908952b82f98dcd73013912ff3bb
xLOKRAYx <jpgp14s06@gmail.com>
2024-02-09 08:30:05 +0100

    finished both scripts

--- file-img.py
+++ file-img.py
@@ -19,20 +19,21 @@ with open(filePath, "rb") as file:
         # Reading the second byte of the set
         file.seek(offset)
         byte2 = int.from_bytes(file.read(1), "big")
-        offset += 2  # Fix: Increment by 2 instead of 1
+        offset += 1  # Fix: Increment by 2 instead of 1
 
         # Reading the third byte of the set
         file.seek(offset)
         byte3 = int.from_bytes(file.read(1), "big")
-        offset += 3  # Fix: Increment by 3 instead of 1
+        offset += 1  # Fix: Increment by 3 instead of 1
 
-        # Setting the pixel
-        pixel = (byte1, byte2, byte3)
+        # Setting the pixel with full transparency
+        pixel = (byte1, byte2, byte3, 0)  # Fix: Set alpha channel to 0 for full transparency
         pixelCoords = (random.randint(0, imgX - 1), random.randint(0, imgY - 1))  # Fix: Adjusted coordinates range
         img.putpixel(pixelCoords, pixel)
 
         # Writing the pixel in the lookuptable
         dataCoordinates.append(pixelCoords)
+        open("dataCoordinates", "w").write(str(dataCoordinates))
         print(pixel)
 
 print(dataCoordinates)
--- img-file.py
+++ img-file.py
@@ -3,16 +3,16 @@ from PIL import Image
 resultImagePath = "image1.png"
 outputFilePath = "reconverted_file"
 
-dataCoordinates = [(12, 190), (361, 73), (485, 429), (131, 50), (506, 497)]
+# Read coordinates from the file and parse the string representation of the list
+with open("dataCoordinates", "r") as f:
+    dataCoordinates = eval(f.read())
 
 img = Image.open(resultImagePath)
-imgX = img.width
-imgY = img.height
+imgX, imgY = img.size
 
 reconverted_data = bytearray()
 
-for coord in dataCoordinates:
-    x, y = coord
+for x, y in dataCoordinates:
     pixel = img.getpixel((x, y))
     reconverted_data.extend([pixel[0], pixel[1], pixel[2]])
 
